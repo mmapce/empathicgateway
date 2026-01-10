528830a9882f146823652fbcaf062bd7a2982ba7
muratkorkmaz <muratkorkmaz5354@gmail.com>
2026-01-10 07:38:41 +0300

    chore: Remove local debug scripts and artifacts from repo (maintain locally)

--- scripts/trigger_chat.py
+++
@@ -1,6 +0,0 @@
-import requests
-import json
-try:
-    res = requests.post("http://empathicgateway-backend-1:8081/chat", json={"text": "Just browsing, thanks for the help!"})
-    print(res.text)
-except Exception as e: print(e)
